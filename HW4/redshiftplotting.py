21fc8640b6d3fc2819fce0496a09a641b6f0a818
Josey Hanish <josey.hanish@gmail.com>
2017-10-02 16:49:12 -0500

    finished hw 4

--- HW4/redshiftplotting.py
+++ HW4/redshiftplotting.py
@@ -1,4 +1,5 @@
 """
+Josey Hanish
 This script will plot redshift vs lookback time according to Hogg Eqn 30, 14
 """
 
@@ -22,7 +23,7 @@ def _integrand(z, om, ok, ol):
 def lookback_time(z, th, om, ok, ol):
     """
     z: redshift
-    th: Age of universe
+    th: Hubble time, inverse of Hubble constant
     om: Density ratio of matter
     ok: Density ratio of something?
     ok: Density ration of cosmological constant
@@ -30,7 +31,12 @@ def lookback_time(z, th, om, ok, ol):
     return th*integral(_integrand, 0, z, om, ok, ol)
 
 if __name__ == '__main__':
-    th=13.88
+    t_universe = 13.88
+#    Hubble time = 72 km/(s Mpc)
+# There are 3.2e19 km per Mpc
+# There are 3.2e16 seconds per Gigayear
+    th = (1./72.)*(3.2*10**19)/(3.2*10**16) #unit Gigayears
+    print th
     om = 0.28
     ok = 0
     ol = 0.72
@@ -40,8 +46,10 @@ if __name__ == '__main__':
         y.append(lookback_time(n, th, om, ok, ol))
     plt.figure()
     plt.plot(x,y)
+    plt.title('Lookback Time vs Redshift')
     plt.xlabel('Redshift z')
-    plt.ylabel('Lookback Time tL')
-#    plt.text(u'tL = tH\u222b', 5, 12)
+    plt.ylabel('Lookback Time tL (Gigayears)')
+    plt.text(2.5, 0.002, u'tL = tH \u222b dz / '
+             u'[(1+z)(\u03a9M(1+z)^3+\u03a9K(1+z)+\u03a9\u039b)^1/2]')
+    plt.savefig('LookbackvsRedshiftJoseyHanish.pdf')
     plt.show()
-    print u'u\8747'
